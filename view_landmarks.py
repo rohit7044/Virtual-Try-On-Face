32fd5e813a68c174820879289bfcc0b72f8ed793
ROHIT DAS <rdas.879@gmail.com>
2024-07-24 15:54:00 +0530

    Created function, test case and reorganized

--- view_landmarks.py
+++ view_landmarks.py
@@ -1,39 +1,43 @@
-# author : ROHIT DAS
 import cv2
 import mediapipe
-from math import sqrt
-import numpy as np
 
-#CONSTANTS
+# CONSTANTS
 
-# landmarks from mesh_map.jpg
-LEFT_EYE = [362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385, 384, 398]
+# Landmarks from mesh_map.jpg
+LEFT_EYE = [362, 382, 381, 380, 374, 373, 390, 249, 246, 466, 388, 387, 386, 385, 384, 398]
 RIGHT_EYE = [33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161, 246]
 
 mediapipe_face_mesh = mediapipe.solutions.face_mesh
-face_mesh = mediapipe_face_mesh.FaceMesh(max_num_faces=1, min_detection_confidence=0.6, min_tracking_confidence=0.7)
-
-video_capture = cv2.VideoCapture(0)
 
 
-while True:
-    _, frame = video_capture.read()
-    frame = cv2.flip(frame, 1)
+def detect_eyes(frame):
+    face_mesh = mediapipe_face_mesh.FaceMesh(max_num_faces=1, min_detection_confidence=0.6, min_tracking_confidence=0.7)
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     output = face_mesh.process(rgb_frame)
     landmark_points = output.multi_face_landmarks
     if landmark_points:
         landmarks = landmark_points[0].landmark
         for i in LEFT_EYE:
-            cv2.circle(frame, (int(landmarks[i].x * frame.shape[1]), int(landmarks[i].y * frame.shape[0])), 1, (0, 0, 255), 1)
+            cv2.circle(frame, (int(landmarks[i].x * frame.shape[1]), int(landmarks[i].y * frame.shape[0])), 1,
+                       (0, 0, 255), 1)
 
         for i in RIGHT_EYE:
-            cv2.circle(frame, (int(landmarks[i].x * frame.shape[1]), int(landmarks[i].y * frame.shape[0])), 1, (0, 0, 255), 1)
+            cv2.circle(frame, (int(landmarks[i].x * frame.shape[1]), int(landmarks[i].y * frame.shape[0])), 1,
+                       (0, 0, 255), 1)
 
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    return frame
 
-video_capture.release()
-cv2.destroyAllWindows()
+# Test code for view_landmarks.py
 
+# video_capture = cv2.VideoCapture(0)
+#
+# while True:
+#     _, frame = video_capture.read()
+#     frame = cv2.flip(frame, 1)
+#     frame = detect_eyes(frame)
+#     cv2.imshow('frame', frame)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+#
+# video_capture.release()
+# cv2.destroyAllWindows()
